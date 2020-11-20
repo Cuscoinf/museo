af461b5aa22d29e7e7445debf6a03acbb4a5296b
Your Name <you@example.com>
2020-11-20 17:20:24 -0500

    museo 20-11-20

--- resources/views/investigador/index.blade.php
+++ resources/views/investigador/index.blade.php
@@ -19,10 +19,10 @@
                 <a class="nav-link active" id="custom-tabs-one-home-tab" data-toggle="pill" href="#custom-tabs-one-home" role="tab" aria-controls="custom-tabs-one-home" aria-selected="true">Registrar Investigador</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="custom-tabs-one-profile-tab" data-toggle="pill" href="#custom-tabs-one-profile" role="tab" aria-controls="custom-tabs-one-profile" aria-selected="false">Investigadores</a>
+                <a class="nav-link" id="custom-tabs-one-profile-tab" data-toggle="pill" href="#custom-tabs-one-profile" role="tab" aria-controls="custom-tabs-one-profile" aria-selected="false">Lista de Investigadores</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="custom-tabs-one-messages-tab" data-toggle="pill" href="#custom-tabs-one-messages" role="tab" aria-controls="custom-tabs-one-messages" aria-selected="false">Crear cuenta</a>
+                <a class="nav-link" id="custom-tabs-one-messages-tab" data-toggle="pill" href="#custom-tabs-one-messages" role="tab" aria-controls="custom-tabs-one-messages" aria-selected="false">Acceso al sistema</a>
             </li>
         </ul>
     </div>
@@ -36,137 +36,172 @@
                 <div class="card-body">
                   <form class="form-horizontal" action="{{route('investigador.store')}}" method="POST">
                       @csrf
-                      <div class="card-body">
-                          <div class="form-group">
-                              <label for="nombre">Nombre</label>
-                              <input type="text" class="form-control" id="nombre" placeholder="Ingrese nombre..." name="nombre" value="{{old('nombre')}}">
-                              @error('nombre')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                      <div class="card-body row">
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="nombre">Nombre</label>
+                                <input type="text" class="form-control" id="nombre" placeholder="Ingrese nombre..." name="nombre" value="{{old('nombre')}}">
+                                @error('nombre')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="apPaterno">Apellido Paterno</label>
-                              <input type="text" class="form-control" id="apPaterno" placeholder="Ingrese apellido paterno" name="apPaterno" value="{{old('apPaterno')}}">
-                              @error('apPaterno')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="apPaterno">Apellido Paterno</label>
+                                <input type="text" class="form-control" id="apPaterno" placeholder="Ingrese apellido paterno" name="apPaterno" value="{{old('apPaterno')}}">
+                                @error('apPaterno')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="apMaterno">Apellido Materno</label>
-                              <input type="text" class="form-control" id="apMaterno" placeholder="Ingrese apellido materno" name="apMaterno" value="{{old('apMaterno')}}">
-                              @error('apMaterno')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="apMaterno">Apellido Materno</label>
+                                <input type="text" class="form-control" id="apMaterno" placeholder="Ingrese apellido materno" name="apMaterno" value="{{old('apMaterno')}}">
+                                @error('apMaterno')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="tipoUsuario">Tipo Usuario del Sistema</label>
-                              <input type="text" class="form-control" id="tipoUsuario" placeholder="Ingrese tipo usuario" name="tipoUsuario" value="{{old('tipoUsuario')}}">
-                              @error('tipoUsuario')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="tipoUsuario">Tipo Usuario del Sistema</label>
+                                <input type="text" class="form-control" id="tipoUsuario" placeholder="Ingrese tipo usuario" name="tipoUsuario" value="{{old('tipoUsuario')}}">
+                                @error('tipoUsuario')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="email">Email</label>
-                              <input type="text" class="form-control" id="email" placeholder="Ingrese email" name="email" value="{{old('email')}}">
-                              @error('email')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="text" class="form-control" id="email" placeholder="Ingrese email" name="email" value="{{old('email')}}">
+                                @error('email')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="tipoDocumento">Tipo Documento</label>
-                              <input type="text" class="form-control" id="tipoDocumento" placeholder="Ingrese tipo documento" name="tipoDocumento" value="{{old('tipoDocumento')}}">
-                              @error('tipoDocumento')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="tipoDocumento">Tipo Documento</label>
+                                <input type="text" class="form-control" id="tipoDocumento" placeholder="Ingrese tipo documento" name="tipoDocumento" value="{{old('tipoDocumento')}}">
+                                @error('tipoDocumento')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="nroDocumento">Nro de Documento</label>
-                              <input type="text" class="form-control" id="nroDocumento" placeholder="Ingrese nro de documento" name="nroDocumento" value="{{old('nroDocumento')}}">
-                              @error('nroDocumento')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="nroDocumento">Nro de Documento</label>
+                                <input type="text" class="form-control" id="nroDocumento" placeholder="Ingrese nro de documento" name="nroDocumento" value="{{old('nroDocumento')}}">
+                                @error('nroDocumento')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="pais">pais</label>
-                              <input type="text" class="form-control" id="pais" placeholder="Ingrese pais" name="pais" value="{{old('pais')}}">
-                              @error('pais')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="pais">pais</label>
+                                <input type="text" class="form-control" id="pais" placeholder="Ingrese pais" name="pais" value="{{old('pais')}}">
+                                @error('pais')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="genero">Genero</label>
-                              <input type="text" class="form-control" id="genero" placeholder="Ingrese genero" name="genero" value="{{old('genero')}}">
-                              @error('genero')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="genero">Genero</label>
+                                <input type="text" class="form-control" id="genero" placeholder="Ingrese genero" name="genero" value="{{old('genero')}}">
+                                @error('genero')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="estado">Estado</label>
-                              <input type="text" class="form-control" id="estado" placeholder="Ingrese estado" name="estado" value="{{old('estado')}}">
-                              @error('estado')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="estado">Estado</label>
+                                <input type="text" class="form-control" id="estado" placeholder="Ingrese estado" name="estado" value="{{old('estado')}}">
+                                @error('estado')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="direccion">Direccion</label>
-                              <input type="text" class="form-control" id="direccion" placeholder="Ingrese direccion" name="direccion" value="{{old('direccion')}}">
-                              @error('direccion')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="direccion">Direccion</label>
+                                <input type="text" class="form-control" id="direccion" placeholder="Ingrese direccion" name="direccion" value="{{old('direccion')}}">
+                                @error('direccion')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="telefono">Telefono</label>
-                              <input type="text" class="form-control" id="telefono" placeholder="Ingrese telefono" name="telefono" value="{{old('telefono')}}">
-                              @error('telefono')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="telefono">Telefono</label>
+                                <input type="text" class="form-control" id="telefono" placeholder="Ingrese telefono" name="telefono" value="{{old('telefono')}}">
+                                @error('telefono')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="formacion_academica_id">Formacion Academica</label>
-                              <input type="text" class="form-control" id="formacion_academica_id" placeholder="Ingrese formacion academica" name="formacion_academica_id" value="{{old('formacion_academica_id')}}">
-                              @error('formacion_academica_id')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="formacion_academica_id">Formacion Academica</label>
+                                <select name="formacion_academica" id="" class="form-control">
+                                    @foreach ($formacion as $f)
+                                    <option value="{{$f->id}}">{{$f->gradoAcademico." - ".$f->universidad." - ".$f->anioTitulacion}}</option>
+                                    @endforeach
+                                </select>
+                                @error('formacion_academica_id')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
-                          <div class="form-group">
-                              <label for="area_id">Area</label>
-                              <input type="text" class="form-control" id="area_id" placeholder="Ingrese area" name="area_id" value="{{old('area_id')}}">
-                              @error('area_id')
-                              <br>
-                              <small>*{{$message}}</small>
-                              <br>
-                               @enderror
+                          <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="area_id">Area</label>
+                                <select name="area_id" class="form-control">
+                                    @foreach ($areas as $area)
+                                    <option value="{{$area->id}}">{{$area->nombre}}</option>
+                                    @endforeach
+                                </select>
+                                @error('area_id')
+                                <br>
+                                <small>*{{$message}}</small>
+                                <br>
+                                 @enderror
+                            </div>
                           </div>
                       </div>
-                      <div class="modal-footer justify-content-between">
-                          <button type="submit" class="btn btn-info">Guardar</button>
-                          <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                      <div class="text-center">
+                          <button type="submit" class="btn btn-info"><i class="fa fa-save"></i> Registrar investigador</button>
                       </div>
                   </form>
                 </div>
